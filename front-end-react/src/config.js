1e217b6d99952a5d4115f352d9906523af3f7462
ddufort1 <ddufort1@verizon.net>
2024-07-31 21:38:07 +0000

    adding a unittests buildspec

--- front-end-react/src/config.js
+++ front-end-react/src/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	WebsocketEndpoint: 'wss://cij2wa0cz6.execute-api.us-west-2.amazonaws.com/Prod'
+	WebsocketEndpoint: 'wss://u6i8u4pdz2.execute-api.us-west-2.amazonaws.com/Prod'
 };
